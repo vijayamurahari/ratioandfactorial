5c14d8abd0b907e6d3b7ba3fff3c0b3128a802e9
vijaya <vijayamurahari@gmail.com>
2023-08-21 10:41:31 +0000

    ratioandfactorial

---
+++ utilities/ratioFactorial/index.js
@@ -0,0 +1,8 @@
+const ratioTwoNumbers = require("../ratio/index");
+const factorialOfNumber = require("../factorial/index");
+const ratioAndFactorial = (num1, num2, num3) => {
+  const ratio = ratioTwoNumbers(num1, num2);
+  const factorial = factorialOfNumber(num3);
+  return { ratio, factorial };
+};
+module.exports = ratioAndFactorial;
